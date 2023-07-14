f95064b0e6d8e2918ed01c320adcdc651e588d4a
akinladenaomi <akinladenaomi>
2023-07-13 23:54:44 -0700

    updated

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
-* Main function is the entry point
-*
-* Return: 0 (Success)
-*/
+ * Main function is the entry point
+ *
+ * Return: 0 (Success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
